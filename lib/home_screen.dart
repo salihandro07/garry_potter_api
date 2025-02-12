c96cbc1f7c65e53faf22ba7595f8edc0d42c6490
salihandro07 <khamrayumovsalih2gmail.com>
2025-02-13 00:22:19 +0600

    init commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,14 +9,39 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     request();
-    return Column(children: [],);
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      body: SafeArea(child: Column(
+        children: [
+          FutureBuilder(
+              future: request(),
+              builder: (context, snapshot) {
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height - 50,
+                  width: MediaQuery.of(context).size.width,
+                  child: ListView.builder(itemBuilder: (context, index) {
+                    final image = snapshot.data![index];
+                    return SizedBox(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        child: Image.network(image));
+                  }, itemCount: snapshot.data!.length),
+                );
+              }
+          ),
+        ],
+      )),
+      );
   }
 
 
-  void request() async {
+  Future<List<String>> request() async {
     final dio = Dio();
     Response response;
     response = await dio.get('https://potterapi-fedeperin.vercel.app/en/characters');
+    List<String> images = response.data.map<String>((
+        item) => item['image'] as String).toList();
     print(response.data.toString());
+    return images;
   }
 }
